78782ebeddfbc7932b10265580dbfef28b817205
wuwenxi <wenxi.wu@hand-china.com>
2020-11-27 11:07:33 +0800

    [fix] 多线程工具类修改

--- g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/util/ThreadPoolExecutorUtil.java
+++ g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/util/ThreadPoolExecutorUtil.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
@@ -24,6 +26,16 @@ public final class ThreadPoolExecutorUtil {
     private ThreadPoolExecutorUtil() {
     }
 
+    /**
+     * 拆分数组
+     *
+     * @param data       执行数据
+     * @param maxThread  最大线程数
+     * @param threadSize 拆分的每个数组大小
+     * @param executor   线程池
+     * @param consumer   执行函数
+     * @param <T>        数据类型
+     */
     public static <T> void runTask(List<T> data, int maxThread, int threadSize, ThreadPoolTaskExecutor executor, Consumer<List<T>> consumer) {
         List<List<T>> splitList = CollectionUtils.splitList(data, threadSize);
         if (splitList == null) {
@@ -36,6 +48,16 @@ public final class ThreadPoolExecutorUtil {
         }
     }
 
+
+    /**
+     * 不拆分数组
+     *
+     * @param data      执行数据
+     * @param maxThread 最大线程数
+     * @param executor  线程池
+     * @param consumer  执行函数
+     * @param <T>       数据类型
+     */
     public static <T> void runTask(List<T> data, int maxThread, ThreadPoolTaskExecutor executor, Consumer<T> consumer) {
         if (CollectionUtils.isEmpty(data)) {
             return;
@@ -44,21 +66,36 @@ public final class ThreadPoolExecutorUtil {
         AtomicInteger taskCounter = new AtomicInteger(0);
         List<Future<String>> result = new ArrayList<>();
         data.forEach(processData -> result.add(taskRunner(processData, executor, maxThread, taskCounter, consumer)));
+        execute(result, countDownLatch);
+    }
 
-        for (Future<String> future : result) {
-            try {
-                future.get();
-            } catch (Exception e) {
-                log.error("");
-            } finally {
-                countDownLatch.countDown();
-            }
+    /**
+     * 数据分组
+     *
+     * @param data      执行数据
+     * @param maxThread 最大线程数
+     * @param executor  线程池
+     * @param consumer  执行函数
+     * @param <E>       key
+     * @param <T>       value
+     */
+    public static <E, T> void runTask(Map<E, T> data, int maxThread, ThreadPoolTaskExecutor executor, Consumer<Map<E, T>> consumer) {
+        if (null == data || data.size() < 1) {
+            return;
         }
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            log.error("");
+        if (data.size() == 1) {
+            consumer.accept(data);
+            return;
         }
+        CountDownLatch countDownLatch = new CountDownLatch(data.size());
+        AtomicInteger taskCounter = new AtomicInteger(0);
+        List<Future<String>> result = new ArrayList<>();
+        data.forEach((key, value) -> {
+            Map<E, T> map = new HashMap<>(1);
+            map.put(key, value);
+            result.add(taskRunner(map, executor, maxThread, taskCounter, consumer));
+        });
+        execute(result, countDownLatch);
     }
 
     private static <T> Future<String> taskRunner(T processData, ThreadPoolTaskExecutor executor, int maxThread, AtomicInteger taskCounter, Consumer<T> consumer) {
@@ -77,6 +114,23 @@ public final class ThreadPoolExecutorUtil {
             if (taskCounter.get() < maxThread) {
                 return executor.submit(taskRunner);
             }
+        }
+    }
+
+    private static void execute(List<Future<String>> result, CountDownLatch countDownLatch) {
+        for (Future<String> future : result) {
+            try {
+                future.get();
+            } catch (Exception e) {
+                log.error("");
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            log.error("");
         }
     }
 }
