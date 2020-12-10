1569888674343f72a2ff231028b0cef6abd62bb7
wuwenxi <wenxi.wu@hand-china.com>
2020-12-10 14:48:04 +0800

    [fix] 修改事物执行器

--- g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/helper/TransactionalHelper.java
+++ g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/helper/TransactionalHelper.java
@@ -3,6 +3,8 @@ package org.g2.core.helper;
 import org.g2.core.bean.Operation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.function.Supplier;
+
 /**
  * 事务操作
  *
@@ -13,5 +15,10 @@ public class TransactionalHelper {
     @Transactional(rollbackFor = Exception.class)
     public void operation(Operation operation) {
         operation.operation();
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public <T> T operation(Supplier<T> supplier) {
+        return supplier.get();
     }
 }
