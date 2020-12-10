3db4b56b5c0cbe9648b6515d549ccaf2549e51f5
wuwenxi <wenxi.wu@hand-china.com>
2020-12-10 14:42:55 +0800

    [fix] 修改异步任务执行器

--- g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/helper/AsyncTaskHelper.java
+++ g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/helper/AsyncTaskHelper.java
@@ -9,7 +9,12 @@ import java.util.function.Consumer;
 
 /**
  * 执行异步任务
- *
+ * <p>
+ *    主线程启动异步线程，如果异步线程操作了主线程中传入的参数，且异步线程在主线程结束前执行完成，
+ *    则异步线程修改的内容将影响到主线程中的该对象；否则不会对主线程中对象有影响。此外异步线程中
+ *    的异常不会影响到主线程的执行。
+ *    因此使用异步线程，最好处理与主线程无关的任务
+ * </p>
  * @author wenxi.wu@hand-chian.com 2020-12-10
  */
 public class AsyncTaskHelper {
@@ -39,7 +44,8 @@ public class AsyncTaskHelper {
      * @param <T>      数据类型
      */
     @Async("asyncTaskExecutor")
-    <T> void operation(T data, Consumer<T> consumer) {
+    @Deprecated
+    public <T> void operation(T data, Consumer<T> consumer) {
         if (log.isDebugEnabled()) {
             log.debug("async task start ,current thread name :{}", Thread.currentThread().getName());
         }
