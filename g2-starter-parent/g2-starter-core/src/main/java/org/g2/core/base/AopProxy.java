3ad70584e3141442ec64e2ea913fa054e7ea70fc
wuwenxi <wenxi.wu@hand-china.com>
2021-03-02 11:27:00 +0800

    [fix] 新增aop代理类生成工具

--- g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/base/AopProxy.java
+++ g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/base/AopProxy.java
@@ -1,25 +1,10 @@
 package org.g2.core.base;
 
-import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.AopContext;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 /**
- * <p>
- * 场景：
- * Service中A方法调用B方法，A方法未使用@Transactional注解而B方法使用@Transactional注解，调用过程中发生异常，B方法事务不生效.
- * <p>
- * 原理解析：
- * spring 在扫描bean的时候会扫描方法上是否包含{@link Transactional}注解，
- * spring会为包含该注解的bean动态地生成一个子类（即代理类，proxy），代理类是继承原来那个bean的。
- * a.当标有@Transactional注解的方法被外部直接调用时，实际上是代理类负责调用(查看
- * {@link org.springframework.aop.framework.CglibAopProxy.DynamicAdvisedInterceptor}调用过程)
- * 代理类在调用目标方法执行前会开启事务{@link TransactionInterceptor#invoke(MethodInvocation)}，并在目标方法结束后提交事务。
- * b.当有@Transactional注解的方法被同类中不带@Transactional注解的方法调用时，实际上是注入IOC容器中的bean负责调用，因此不开启事务
- * <p>
- * 使用：Service中不带@Transactional注解的方法调用同类中带有@Transactional注解的方法
- * </p>
+ * proxy()便于获取自身接口代理类，常用在一个事务方法里调用当前类的其它事务方法，
+ * 如果不使用代理对象调用方法，本质使用的是原始对象，因而可能导致事务或AOP拦截不生效
  *
  * @author wenxi.wu@hand-chian.com 2021-03-01
  */
@@ -27,7 +12,7 @@ public interface AopProxy<T> {
 
 
     /**
-     * 生成aop代理对象
+     * 获取aop代理对象
      *
      * @return aop代理对象
      */
