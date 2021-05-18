20f6be223996bb0aa6727a898abcf8efcd16d9dd
wuwenxi <wenxi.wu@hand-china.com>
2021-05-18 21:58:46 +0800

    [fix] redis发布订阅模块修改

--- g2-starter-parent/g2-starter-mq/src/main/java/org/g2/starter/mq/listener/config/ListenerProcessor.java
+++ g2-starter-parent/g2-starter-mq/src/main/java/org/g2/starter/mq/listener/config/ListenerProcessor.java
@@ -4,7 +4,6 @@ import org.g2.starter.mq.MqProcessorCreator;
 import org.g2.starter.mq.infra.constants.MqConstants;
 import org.g2.starter.mq.listener.annotation.Listener;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.Topic;
 
@@ -19,19 +18,13 @@ public class ListenerProcessor extends MqProcessorCreator {
 
     @Override
     protected Collection<? extends Topic> getTopic(Annotation annotation) {
-        if (annotation instanceof Listener) {
-            Listener listener = (Listener) annotation;
-            String db = listener.db() == -1 ? "*" : "" + listener.db();
-            return Collections.singletonList(new PatternTopic(String.format(MqConstants.KEY_EXPIRATION_EVENT, db)));
-        }
-        return null;
+        Listener listener = (Listener) annotation;
+        String db = listener.db() == -1 ? "*" : "" + listener.db();
+        return Collections.singletonList(new PatternTopic(String.format(MqConstants.KEY_EXPIRATION_EVENT, db)));
     }
 
     @Override
     protected Annotation findAnnotation(Object bean) {
-        if (bean instanceof MessageListener) {
-            return AnnotationUtils.findAnnotation(bean.getClass(), Listener.class);
-        }
-        return null;
+        return AnnotationUtils.findAnnotation(bean.getClass(), Listener.class);
     }
 }
--- g2-starter-parent/g2-starter-mq/src/main/java/org/g2/starter/mq/subject/config/SubjectProcessor.java
+++ g2-starter-parent/g2-starter-mq/src/main/java/org/g2/starter/mq/subject/config/SubjectProcessor.java
@@ -3,7 +3,6 @@ package org.g2.starter.mq.subject.config;
 import org.g2.starter.mq.MqProcessorCreator;
 import org.g2.starter.mq.subject.annotation.Subject;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.Topic;
 import org.springframework.util.Assert;
@@ -20,25 +19,19 @@ public class SubjectProcessor extends MqProcessorCreator {
 
     @Override
     protected Collection<? extends Topic> getTopic(Annotation annotation) {
-        if (annotation instanceof Subject) {
-            Subject subject = (Subject) annotation;
-            String[] values = subject.values();
-            Assert.notEmpty(values, "at least one values required in @Subject");
-            List<PatternTopic> patternTopics = new ArrayList<>();
-            for (String value : values) {
-                PatternTopic patternTopic = new PatternTopic(value);
-                patternTopics.add(patternTopic);
-            }
-            return patternTopics;
+        Subject subject = (Subject) annotation;
+        String[] values = subject.values();
+        Assert.notEmpty(values, "at least one values required in @Subject");
+        List<PatternTopic> patternTopics = new ArrayList<>();
+        for (String value : values) {
+            PatternTopic patternTopic = new PatternTopic(value);
+            patternTopics.add(patternTopic);
         }
-        return null;
+        return patternTopics;
     }
 
     @Override
     protected Annotation findAnnotation(Object bean) {
-        if (bean instanceof MessageListener) {
-            return AnnotationUtils.findAnnotation(bean.getClass(), Subject.class);
-        }
-        return null;
+        return AnnotationUtils.findAnnotation(bean.getClass(), Subject.class);
     }
 }
