3d6767d4af261856e84c161a692fb662bbb79510
wuwenxi <wenxi.wu@hand-china.com>
2021-07-16 16:00:01 +0800

    [fix] fix

--- g2-starter-parent/g2-starter-mq/src/main/java/org/g2/starter/mq/EnableMq.java
+++ g2-starter-parent/g2-starter-mq/src/main/java/org/g2/starter/mq/EnableMq.java
@@ -2,7 +2,6 @@ package org.g2.starter.mq;
 
 import org.g2.starter.mq.listener.annotation.Listener;
 import org.g2.starter.mq.config.spring.register.MqProcessorRegister;
-import org.g2.starter.mq.publisher.annotation.Publisher;
 import org.g2.starter.mq.subject.annotation.Subject;
 import org.springframework.context.annotation.Import;
 
@@ -13,7 +12,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 开启基于注解的redis发布订阅 {@link Publisher,Subject}及redis监听 {@link Listener}
+ * 开启基于注解的redis发布订阅 {@link Subject}及redis监听 {@link Listener}
  *
  * @author wenxi.wu@hand-chian.com 2021-05-17
  */
