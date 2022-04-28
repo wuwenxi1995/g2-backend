92278250088cad30587160cbcc0b0a900cba68d2
wuwenxi <wenxi.wu@hand-china.com>
2022-04-28 14:39:10 +0800

    [fix] 修改数据去重工具

--- g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/util/DataUniqueUtil.java
+++ g2-starter-parent/g2-starter-core/src/main/java/org/g2/core/util/DataUniqueUtil.java
@@ -22,116 +22,53 @@ public class DataUniqueUtil {
     private DataUniqueUtil() {
     }
 
+    /**
+     * 数据去重
+     */
     public static <K, T> Map<K, T> filter(List<T> operationDataList, DataUniqueHandler<K, T> handler) {
         if (CollectionUtils.isEmpty(operationDataList)) {
             return null;
         }
-        boolean parallel = handler.parallel();
-        Stream<T> stream = parallel ? operationDataList.parallelStream() : operationDataList.stream();
-        Map<K, List<T>> groupMap = stream.collect(Collectors.groupingBy(handler::mapKey));
-        Map<K, T> result;
-        if (parallel && groupMap.size() > handler.batchSize()) {
-            ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
-            result = new ConcurrentHashMap<>(groupMap.size() << 1);
-            ParallelTask<K, T> task = new ParallelTask<>(new ArrayList<>(groupMap.values()), result, handler);
-            forkJoinPool.submit(task);
-        } else {
-            result = new HashMap<>(groupMap.size() << 1);
-            groupMap.forEach((key, value) -> handler(key, value, result, handler));
-        }
+        Map<K, T> result = new HashMap<>();
+        operationDataList.parallelStream()
+                .filter(handler::filter)
+                .collect(Collectors.groupingBy(handler::mapKey))
+                .entrySet().parallelStream()
+                .forEach((entry) -> {
+                    T choose;
+                    K key = entry.getKey();
+                    List<T> value = entry.getValue();
+                    if (value.size() > 1) {
+                        // 选择要处理的数据
+                        choose = handler.choose(value);
+                        if (choose == null) {
+                            return;
+                        }
+                        result.put(key, choose);
+                        // 处理脏数据
+                        List<T> filter = value.stream().filter(e -> !e.equals(choose)).collect(Collectors.toList());
+                        handler.abandonData(filter);
+                    } else {
+                        choose = value.get(0);
+                        result.put(key, choose);
+                    }
+                    // 处理已选择的数据
+                    handler.handler(choose);
+                });
         return result;
     }
 
-    public static <K, T> void filterAndHandler(List<T> operationDataList, DataUniqueHandler<K, T> handler) {
-        if (CollectionUtils.isEmpty(operationDataList)) {
-            return;
-        }
-        boolean parallel = handler.parallel();
-        Stream<T> stream = parallel ? operationDataList.parallelStream() : operationDataList.stream();
-        Map<K, List<T>> groupMap = stream.collect(Collectors.groupingBy(handler::mapKey));
-        if (parallel && groupMap.size() > handler.batchSize()) {
-            ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
-            ParallelTask<K, T> task = new ParallelTask<>(new ArrayList<>(groupMap.values()), handler);
-            forkJoinPool.submit(task);
-        } else {
-            groupMap.forEach((key, value) -> handler(value, handler));
-        }
-    }
-
-    private static class ParallelTask<K, T> extends RecursiveAction {
-
-        private List<List<T>> operationDataList;
-        private Map<K, T> result;
-        private DataUniqueHandler<K, T> handler;
-
-        ParallelTask(List<List<T>> operationDataList, Map<K, T> result, DataUniqueHandler<K, T> handler) {
-            this(operationDataList, handler);
-            this.result = result;
-        }
-
-        ParallelTask(List<List<T>> operationDataList, DataUniqueHandler<K, T> handler) {
-            this.operationDataList = operationDataList;
-            this.handler = handler;
-        }
-
-        @Override
-        protected void compute() {
-            int batchSize = handler.batchSize();
-            if (operationDataList.size() < batchSize) {
-                for (List<T> value : operationDataList) {
-                    if (result != null) {
-                        handler(handler.mapKey(value.get(0)), value, result, handler);
-                    } else {
-                        handler(value, handler);
-                    }
-                }
-            } else {
-                int index = operationDataList.size() >> 1;
-                ParallelTask<K, T> left = new ParallelTask<>(operationDataList.subList(0, index), result, handler);
-                ParallelTask<K, T> right = new ParallelTask<>(operationDataList.subList(index + 1, operationDataList.size()), result, handler);
-                invokeAll(left, right);
-            }
-        }
-    }
-
-    private static <K, T> void handler(K key, List<T> value, Map<K, T> result, DataUniqueHandler<K, T> handler) {
-        if (value.size() > 1) {
-            // 选择要处理的数据
-            T choose = handler.choose(value);
-            if (choose == null) {
-                return;
-            }
-            result.put(key, choose);
-            // 处理脏数据
-            List<T> filter = value.stream().filter(e -> !e.equals(choose)).collect(Collectors.toList());
-            handler.abandonData(filter);
-            // 处理已选择的数据
-            handler.handler(choose);
-        } else {
-            result.put(key, value.get(0));
-        }
-    }
-
-    private static <K, T> void handler(List<T> value, DataUniqueHandler<K, T> handler) {
-        T choose;
-        if (value.size() > 1) {
-            // 选择要处理的数据
-            choose = handler.choose(value);
-            if (choose == null) {
-                return;
-            }
-            // 处理脏数据
-            List<T> filter = value.stream().filter(e -> !e.equals(choose)).collect(Collectors.toList());
-            handler.abandonData(filter);
-        } else if (value.size() == 1) {
-            choose = value.get(0);
-        } else {
-            choose = null;
-        }
-        if (choose != null) {
-            // 处理已选择的数据
-            handler.handler(choose);
-        }
+    /**
+     * 筛选重复元素
+     */
+    public static <K, T> List<K> filterDumpData(List<T> operationDatum, DumpDataHandler<K, T> dumpDataHandler) {
+        return operationDatum.stream()
+                .filter(dumpDataHandler::filter)
+                .collect(Collectors.groupingBy(dumpDataHandler::mapKey))
+                .entrySet().parallelStream()
+                .filter(e -> e.getValue().size() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public interface DataUniqueHandler<K, T> {
@@ -167,21 +104,33 @@ public class DataUniqueUtil {
         }
 
         /**
-         * 是否执行并行任务
+         * 需要过滤的数据
          *
-         * @return true
+         * @param value 值
+         * @return true/false
          */
-        default boolean parallel() {
+        default boolean filter(T value) {
             return true;
         }
+    }
+
+    public interface DumpDataHandler<K, T> {
+        /**
+         * 返回需要分组的健
+         *
+         * @param value 值
+         * @return 健
+         */
+        K mapKey(T value);
 
         /**
-         * 批数据量
+         * 需要过滤的数据
          *
-         * @return int
+         * @param value 值
+         * @return true/false
          */
-        default int batchSize() {
-            return 1000;
+        default boolean filter(T value) {
+            return true;
         }
     }
 }
