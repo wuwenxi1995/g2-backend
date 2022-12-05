cc426ea34b1fd7bc7d3af92fdb45fd7c9502c155
wuwenxi <wenxi.wu@hand-china.com>
2022-12-05 14:26:39 +0800

    [fix] fix

--- g2-starter-parent/g2-starter-redisson/src/main/java/org/g2/starter/redisson/lock/infra/provider/LockInfoProvider.java
+++ g2-starter-parent/g2-starter-redisson/src/main/java/org/g2/starter/redisson/lock/infra/provider/LockInfoProvider.java
@@ -7,8 +7,12 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.g2.starter.redisson.lock.config.RedissonConfigureProperties;
 import org.g2.starter.redisson.lock.domain.LockInfo;
 import org.g2.starter.redisson.lock.infra.annotation.Lock;
+import org.g2.starter.redisson.lock.infra.annotation.LockKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.expression.MethodBasedEvaluationContext;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -17,6 +21,8 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -28,6 +34,7 @@ public class LockInfoProvider {
     private static final Logger log = LoggerFactory.getLogger(LockInfoProvider.class);
 
     private final ExpressionParser parser = new SpelExpressionParser();
+    private ParameterNameDiscoverer nameDiscoverer = new DefaultParameterNameDiscoverer();
     private final RedissonConfigureProperties redissonConfigureProperties;
 
     public LockInfoProvider(RedissonConfigureProperties redissonConfigureProperties) {
@@ -59,8 +66,9 @@ public class LockInfoProvider {
                 }
             }
         } else {
-            // redis锁名称，默认：类名+方法名
-            lockName = getLockName((MethodSignature) joinPoint.getSignature());
+            // redis锁名称，默认：类名+方法名+keys
+            List<String> keyList = this.getKeyList(joinPoint, lock.keys());
+            lockName = getLockName((MethodSignature) joinPoint.getSignature(), keyList);
         }
         // 获取锁最长等待时间 可通过g2.lock.pattern.wait-time配置
         long waitTime = getLockTime(lock);
@@ -82,8 +90,9 @@ public class LockInfoProvider {
         return method;
     }
 
-    private String getLockName(MethodSignature signature) {
-        return String.format("%s.%s", signature.getDeclaringTypeName(), signature.getMethod().getName());
+    private String getLockName(MethodSignature signature, List<String> keyList) {
+        return String.format("%s.%s.%s", signature.getDeclaringTypeName(), signature.getMethod().getName(),
+                org.springframework.util.StringUtils.collectionToDelimitedString(keyList, "", "-", ""));
     }
 
     private long getLockTime(Lock lock) {
@@ -92,5 +101,53 @@ public class LockInfoProvider {
 
     private long getLeaseTime(Lock lock) {
         return lock.leaseTime() < 0 ? redissonConfigureProperties.getProperty().getLeaseTime() : lock.leaseTime();
+    }
+
+    private List<String> getKeyList(ProceedingJoinPoint joinPoint, String[] keys) {
+        Method method = getMethod(joinPoint);
+        List<String> keyList = new ArrayList<>();
+        if (keys.length != 0) {
+            List<String> spelDefinitionKey = getSpelDefinitionKey(keys, method, joinPoint.getArgs());
+            keyList.addAll(spelDefinitionKey);
+        }
+        List<String> parameterKeys = getParameterKey(method.getParameters(), joinPoint.getArgs());
+        keyList.addAll(parameterKeys);
+        return keyList;
+    }
+
+    private List<String> getSpelDefinitionKey(String[] definitionKeys, Method method, Object[] parameterValues) {
+        List<String> definitionKeyList = new ArrayList<>();
+        for (String definitionKey : definitionKeys) {
+            EvaluationContext context =
+                    new MethodBasedEvaluationContext(null, method, parameterValues, nameDiscoverer);
+            Object value = parser.parseExpression(definitionKey).getValue(context);
+            if (value != null) {
+                definitionKeyList.add(value.toString());
+            }
+        }
+        return definitionKeyList;
+    }
+
+    /**
+     * 获取参数KEY
+     */
+    private List<String> getParameterKey(Parameter[] parameters, Object[] parameterValues) {
+        List<String> parameterKey = new ArrayList<>();
+        for (int i = 0; i < parameters.length; i++) {
+            // 单个LockKey?
+            if (parameters[i].getAnnotation(LockKey.class) != null) {
+                LockKey keyAnnotation = parameters[i].getAnnotation(LockKey.class);
+                if (keyAnnotation.value().isEmpty()) {
+                    parameterKey.add(parameterValues[i].toString());
+                } else {
+                    StandardEvaluationContext context = new StandardEvaluationContext(parameterValues[i]);
+                    Object value = parser.parseExpression(keyAnnotation.value()).getValue(context);
+                    if (value != null) {
+                        parameterKey.add(value.toString());
+                    }
+                }
+            }
+        }
+        return parameterKey;
     }
 }
